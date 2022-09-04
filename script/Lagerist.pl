9c0f35cbeaface2ddfccec5951a10e8a02c97482
Alexander Thiel <alex@thiel-online.net>
2022-09-04 19:13:47 +0200

    Loglevel is now configurable using the --loglevel option and the base stock data file may be specified using --stock=<XLS-file>

--- script/Lagerist.pl
+++ script/Lagerist.pl
@@ -9,36 +9,45 @@ use Log::Log4perl;
 use Log::Log4perl::Level;
 use Log::Log4perl::Logger;
 
-use Data::Dumper;
-
 use Spreadsheet::Read qw(ReadData);
 use Spreadsheet::WriteExcel;
 
+use Getopt::Long;
+
+use Data::Dumper; # TODO: Remove debug stuff
+
+# Processing the command line options
+GetOptions(
+    'loglevel=s' => \(my $log_level = 'INFO'),
+    'stock=s'      => \(my $opt_stock_reference = './data/stock-001.xlsx'), # Removing end brackets, inside continuous terminal strips
+) or die "Invalid options passed to $0\n";
+
 # Initializing the logging mechanism
-# TODO: Make log level configurable
-Log::Log4perl->easy_init( Log::Log4perl::Level::to_priority( uc('INFO') ) );
+Log::Log4perl->easy_init(Log::Log4perl::Level::to_priority(uc($log_level)));
 my $logger = Log::Log4perl->get_logger();
 
 # TODO: Should be passed as command line parameter
-my $opt_lager_definition = File::Spec->rel2abs('./data/stock-001.xlsx');
 my $opt_source_file = File::Spec->rel2abs(join(' ', @ARGV));
+
+# Postprocessing command line parameters
+$opt_stock_reference = File::Spec->rel2abs($opt_stock_reference);
 my $opt_storage_file = undef;
 my $opt_nostorage_file = undef;
 {
-    my ($name,$path,$suffix) = fileparse($opt_source_file,('.xlsx', '.xls', '.csv', '.txt'));
+    my ($name, $path, $suffix) = fileparse($opt_source_file, ('.xlsx', '.xls', '.csv', '.txt'));
     $opt_storage_file = File::Spec->catfile($path, "${name}_storage${suffix}");
     $opt_nostorage_file = File::Spec->catfile($path, "${name}_project${suffix}");
 }
 
 # Loading the storage manager information
-$logger->info("Reading storage information from [$opt_lager_definition]");
+$logger->info("Reading storage information from [$opt_stock_reference]");
 my %storage_content;
 {
-    my $storage_data = Spreadsheet::Read::ReadData($opt_lager_definition) or die $!;
+    my $storage_data = Spreadsheet::Read::ReadData($opt_stock_reference) or die $!;
 
     $logger->debug("Analyzing headlines");
     my @headlines = Spreadsheet::Read::row($storage_data->[1], 1);
-    $logger->debug('['.scalar(@headlines).'] Headline elements detected');
+    $logger->debug('[' . scalar(@headlines) . '] Headline elements detected');
 
     $logger->debug("Reading content");
     my @rowsmulti = Spreadsheet::Read::rows($storage_data->[1]);
@@ -63,7 +72,7 @@ my @bom_headlines;
     $logger->debug("Analyzing headlines");
     @bom_headlines = Spreadsheet::Read::row($bom_data->[1], 1);
     push(@bom_headlines, 'Lagerplatz'); # Adding the 'Lagerplatz' headline
-    $logger->debug('['.scalar(@bom_headlines).'] Headline elements detected');
+    $logger->debug('[' . scalar(@bom_headlines) . '] Headline elements detected');
 
     $logger->debug("Reading content");
 
@@ -94,42 +103,44 @@ my $nostorage_spreadsheet = Spreadsheet::WriteExcel->new($opt_nostorage_file);
 my $storage_worksheet = $storage_spreadsheet->add_worksheet();
 my $nostorage_worksheet = $nostorage_spreadsheet->add_worksheet();
 
-my($storage_row, $nostorage_row) = (0, 0);
+my ($storage_row, $nostorage_row) = (0, 0);
 # Writing the headlines
 $logger->debug("Writing headlines to both output spreadsheets");
-for(my $col = 0; $col < scalar(@bom_headlines); $col++) {
+for (my $col = 0; $col < scalar(@bom_headlines); $col++) {
     # TODO: A fancy format should be added to the headlines
     $storage_worksheet->write($storage_row, $col, $bom_headlines[$col]);
     $nostorage_worksheet->write($storage_row, $col, $bom_headlines[$col]);
 }
-$storage_row++; $nostorage_row++;
+$storage_row++;
+$nostorage_row++;
 
 # Iterating the BOM result and writing the content inside the spreadsheets
 foreach my $article_number (keys(%storage_content)) {
-    if( defined($storage_content{$article_number}{'Lagerplatz'}) ) {
+    if (defined($storage_content{$article_number}{'Lagerplatz'})) {
         # Article available in storage
         $logger->debug("Article [$article_number] defined for storage spreadsheet output");
         #print Dumper \$storage_content{$article_number};
-        for(my $col = 0; $col < scalar(@bom_headlines); $col++) {
+        for (my $col = 0; $col < scalar(@bom_headlines); $col++) {
             my $key = $bom_headlines[$col];
             my $value = $bom_content{$article_number}{$bom_headlines[$col]} || '';
 
-            if($key eq 'Lagerplatz') { $value = $storage_content{$article_number}{'Lagerplatz'}; }
-            if($key eq 'Zusatztext 1') { $value = $article_number; } # Per definition specially for UPMANN
+            if ($key eq 'Lagerplatz') {$value = $storage_content{$article_number}{'Lagerplatz'};}
+            if ($key eq 'Zusatztext 1') {$value = $article_number;} # Per definition specially for UPMANN
 
             $logger->debug(" -> Writing [$key]->[$value]");
             $storage_worksheet->write($storage_row, $col, $value);
         }
         $storage_row++;
-    } else {
+    }
+    else {
         # Article not available in storage
         $logger->debug("Article [$article_number] defined for project specific spreadsheet output");
-        for(my $col = 0; $col < scalar(@bom_headlines); $col++) {
+        for (my $col = 0; $col < scalar(@bom_headlines); $col++) {
             my $key = $bom_headlines[$col];
             my $value = $bom_content{$article_number}{$bom_headlines[$col]} || '';
 
-            if($key eq 'Lagerplatz') { $value = $storage_content{$article_number}{'Lagerplatz'}; }
-            if($key eq 'Zusatztext 1') { $value = $article_number; } # Per definition specially for UPMANN
+            if ($key eq 'Lagerplatz') {$value = $storage_content{$article_number}{'Lagerplatz'};}
+            if ($key eq 'Zusatztext 1') {$value = $article_number;} # Per definition specially for UPMANN
             $value = defined($value) ? $value : '';
 
             $logger->debug(" -> Writing [$key]->[$value]");
